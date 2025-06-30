139987b252efde1752d095f0bde99035dabf7cac
mariustankeu <72073503+mariustankeu@users.noreply.github.com>
2025-06-30 17:48:12 +0200

    Update SearchActions.java
    Add Annotation @Step

--- src/test/java/starter/duckduckgo/SearchActions.java
+++ src/test/java/starter/duckduckgo/SearchActions.java
@@ -4,6 +4,7 @@ import net.serenitybdd.core.steps.UIInteractions;
 import org.openqa.selenium.Keys;
 
 public class SearchActions extends UIInteractions {
+    @Step("Search for '{0}'")
     public void byKeyword(String keyword) {
       $("#searchbox_input").sendKeys(keyword, Keys.ENTER);
     }
