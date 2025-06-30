c9a8dc5ee862fa3c5d8c4646b56c0fc9a81a43b2
mariustankeu <72073503+mariustankeu@users.noreply.github.com>
2025-06-30 17:09:45 +0200

    Update WhenSearchingByKeyword.java, adding driver

--- src/test/java/starter/duckduckgo/WhenSearchingByKeyword.java
+++ src/test/java/starter/duckduckgo/WhenSearchingByKeyword.java
@@ -1,11 +1,16 @@
 package starter.duckduckgo;
 
+import net.serenitybdd.annotations.Managed;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
 
-@ExtendWith(SerenityJUnit5Extension.class)              
-class WhenSearchingByKeyword {              
+@ExtendWith(SerenityJUnit5Extension.class)
+class WhenSearchingByKeyword {
+
+    @Managed(driver = "chrome", options = "headless")
+    WebDriver driver;
 
     @Test
     void theKeywordShouldAppearInTheResultsSidebar() {
