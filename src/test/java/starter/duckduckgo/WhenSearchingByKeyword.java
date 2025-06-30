3a31de14da08715a6e028accd1c32aeeb019c919
mariustankeu <72073503+mariustankeu@users.noreply.github.com>
2025-06-30 17:34:32 +0200

    Update WhenSearchingByKeyword.java
    Adding content inside the main function

--- src/test/java/starter/duckduckgo/WhenSearchingByKeyword.java
+++ src/test/java/starter/duckduckgo/WhenSearchingByKeyword.java
@@ -1,10 +1,11 @@
 package starter.duckduckgo;
 
-import net.serenitybdd.annotations.Managed;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import org.junit.jupiter.api.Test;
+import net.thucydides.core.annotations.Managed;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SerenityJUnit5Extension.class)
 class WhenSearchingByKeyword {
@@ -12,8 +13,14 @@ class WhenSearchingByKeyword {
     @Managed(driver = "chrome", options = "headless")
     WebDriver driver;
 
+    NavigateActions navigate;
+    SearchActions search;
+    SearchResultSidebar searchResultSidebar;
+
     @Test
     void theKeywordShouldAppearInTheResultsSidebar() {
-
+        navigate.toTheDuckDuckGoSearchPage();
+        search.byKeyword("Cucumber");
+        assertThat(searchResultSidebar.heading()).isEqualTo("Cucumber");
     }
 }
